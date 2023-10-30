0fb1891d042f847b59bdfcd2c3cf02500b5a2833
Quan Luong <lhongquan.1998@gmail.com>
2023-10-30 21:49:19 +0700

    thumbnail for image only

--- src/core/aws/services/s3.service.ts
+++ src/core/aws/services/s3.service.ts
@@ -11,7 +11,7 @@ import { EventEmitter2 } from '@nestjs/event-emitter';
 import { IStorageService } from '../../../core/types';
 import { generateRandomId } from '../../../utils';
 import * as sharp from 'sharp';
-
+const convert = require('heic-convert');
 @Injectable()
 export class AwsS3Service implements IStorageService {
   private _thumbnailExt = '-thumbnail';
@@ -84,6 +84,13 @@ export class AwsS3Service implements IStorageService {
     return this._getS3UploadResponse(filePath);
   }
   private async _resizeAndUploadImage(file: Express.Multer.File) {
+    if (file.originalname.toLowerCase().includes('heic')) {
+      file.buffer = await convert({
+        buffer: file.buffer, // the HEIC file buffer
+        format: 'JPEG', // output format
+        quality: 1, // the jpeg compression quality, between 0 and 1
+      });
+    }
     const filePath = this._generateFilePath();
     const command = new PutObjectCommand({
       Bucket: process.env.S3_BUCKET,
